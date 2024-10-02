efbd3f7cae11de6bee5dea0c6d19241bb216d32b
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:43:33 +0530

    Adding src/main/java/com/demo/tally_db_sync/TallyDbSyncApplication.java

---
+++ src/main/java/com/demo/tally_db_sync/TallyDbSyncApplication.java
@@ -0,0 +1,13 @@
+package com.demo.tally_db_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallyDbSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallyDbSyncApplication.class, args);
+	}
+
+}
