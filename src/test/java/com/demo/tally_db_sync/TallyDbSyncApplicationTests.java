b89a3e71662f7252c5a32186baf0cfd2031ba6e9
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:43:35 +0530

    Adding src/test/java/com/demo/tally_db_sync/TallyDbSyncApplicationTests.java

---
+++ src/test/java/com/demo/tally_db_sync/TallyDbSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.tally_db_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallyDbSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
