340f918d0143f0c73f2f6af8077a6f9e14273bc6
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:46:06 +0530

    Adding src/test/java/com/code_pusher_2_git/CodePusher2GitApplicationTests.java

---
+++ src/test/java/com/code_pusher_2_git/CodePusher2GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code_pusher_2_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePusher2GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
